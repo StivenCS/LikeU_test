1ed13b8dcf9b4fecb6ff6c8600e52558610011d5
StivenCS <stivl52@hotmail.com>
2021-02-18 23:05:06 -0500

    Documentation

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -79,3 +79,110 @@ class Controller extends BaseController
  *     )
  * )
  */
+
+
+/**
+ * @OA\Post(
+ * path="/api/client/create",
+ * summary="Customer creation",
+ * description="Customer creation",
+ * operationId="client_create",
+ * tags={"clients"},
+ * security={{ "bearer_token": {} }},
+    * @OA\RequestBody(
+    *    required=true,
+    *    description="information required for the client",
+    *    @OA\JsonContent(
+    *       required={"name","identification", "dateOfBirth"},
+    *       @OA\Property(property="name", type="string", format="name", example="like"),
+    *       @OA\Property(property="identification", type="number", format="identification", example="123456"),
+    *       @OA\Property(property="dateOfBirth", type="date", format="dateOfBirth", example="2020-02-10"),
+    *    ),
+    * ),
+ * @OA\Response(
+ *    response=201,
+ *    description="Customer creation",
+ *    @OA\JsonContent(
+ *       @OA\Property(property="message", type="string", example="Customer created!!")
+ *        )
+ *     )
+ * )
+ */
+
+
+/**
+ * @OA\Post(
+ * path="/api/diary/create",
+ * summary="Diary creation",
+ * description="Diary creation",
+ * operationId="diary_create",
+ * tags={"diary"},
+ * security={{ "bearer_token": {} }},
+    * @OA\RequestBody(
+    *    required=true,
+    *    description="information required for the diary",
+    *    @OA\JsonContent(
+    *       required={"matter","date", "status", "client_id"},
+    *       @OA\Property(property="matter", type="string", format="matter", example="like"),
+    *       @OA\Property(property="date", type="date", format="date", example="2020-12-01"),
+    *       @OA\Property(property="status", type="string", format="status", example="Programada"),
+    *       @OA\Property(property="client_id", type="number", format="client_id", example="1,2,3,4..."),
+    *    ),
+    * ),
+ * @OA\Response(
+ *    response=201,
+ *    description="Customer creation",
+ *    @OA\JsonContent(
+ *       @OA\Property(property="message", type="string", example="sheduled!!")
+ *        )
+ *     )
+ * )
+ */
+
+ /**
+* @OA\Put(
+* path="/api/diary/update/{id}",
+* summary="edit diary",
+* description="edit diary",
+* operationId="diary_update",
+* tags={"diary"},
+* security={{ "bearer_token": {} }},
+* @OA\Parameter(
+*    description="diary id",
+*    in="path",
+*    name="id",
+*    required=true,
+*    example="1",
+*    @OA\Schema(
+*       type="integer",
+*       format="int64"
+*    )
+* ),
+* @OA\RequestBody(
+*    required=true,
+*    description="information required for update the diary",
+*    @OA\JsonContent(
+*      required={"matter","date", "status", "client_id"},
+*       @OA\Property(property="matter", type="string", format="matter", example="like"),
+*       @OA\Property(property="date", type="date", format="date", example="2020-12-01"),
+*       @OA\Property(property="status", type="string", format="status", example="Programada"),
+*       @OA\Property(property="client_id", type="number", format="client_id", example="1"),
+*    ),
+* ),
+* @OA\Response(
+*    response=201,
+*    description="response",
+*    @OA\JsonContent(
+*      @OA\Property(property="message", type="string", example="Diary update")
+*    )
+* ),
+*   @OA\Response(
+*       response=400,
+*       description="response",
+*       @OA\JsonContent(
+*           @OA\Property(property="message", type="string", example="Can't update the diary")
+*       )
+*   ),
+*
+*)
+*/
